ea38f1049365304057d799ee48381071829745bd
Illia Maksymov <98290303+imksmv@users.noreply.github.com>
2023-09-09 10:47:15 +0100

    Added table with delete function

--- components/ExpenseList.tsx
+++ components/ExpenseList.tsx
@@ -7,9 +7,12 @@ interface Expense {
 
 interface Props {
   expenses: Expense[];
+  onDelete: (id: number) => void;
 }
 
-export default function ExpenseList({ expenses }: Props) {
+export default function ExpenseList({ expenses, onDelete }: Props) {
+  if (expenses.length === 0) return null;
+
   return (
     <table className="table table-border">
       <thead>
@@ -21,15 +24,35 @@ export default function ExpenseList({ expenses }: Props) {
         </tr>
       </thead>
       <tbody>
-        {expenses.map((expense) => <tr key={expense.id}>
-          <td>{expense.description}</td>
-          <td>{expense.amount}</td>
-          <td>{expense.category}</td>
-          <td>
-            <button className="btn btn-outline-danger"></button>
-          </td>
-        </tr>)}
+        {expenses.map((expense) => (
+          <tr key={expense.id}>
+            <td>{expense.description}</td>
+            <td>{expense.amount}</td>
+            <td>{expense.category}</td>
+            <td>
+              <button
+                className="btn btn-outline-danger"
+                onClick={() => onDelete(expense.id)}
+              >
+                Delete
+              </button>
+            </td>
+          </tr>
+        ))}
       </tbody>
+      <tfoot>
+        <tr>
+          <td>Total</td>
+          <td>
+            $
+            {expenses
+              .reduce((acc, expense) => acc + expense.amount, 0)
+              .toFixed(2)}
+          </td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tfoot>
     </table>
   );
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import Form from "../components/Form.tsx";
 import ExpenseList from "../components/ExpenseList.tsx";
+import { useState } from "react";
 
 export default function App() {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 12, category: "utilities" },
+    { id: 2, description: "bbb", amount: 12, category: "utilities" },
+    { id: 3, description: "ccc", amount: 12, category: "utilities" },
+  ]);
+
   return (
     <>
       <Form />
-      <ExpenseList />
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) =>
+          setExpenses(expenses.filter((expense) => expense.id !== id))
+        }
+      />
     </>
   );
 }
